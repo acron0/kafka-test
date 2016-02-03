07645a7b27127d27b8f80ca69fcaf61f3a6c02ba
Antony Woods <antony@mastodonc.com>
2016-02-03 09:22:37 +0000

    Improvement to run script

--- run_kafka.sh
+++ run_kafka.sh
@@ -4,5 +4,4 @@ docker rm kafka
 docker run -d --name zookeeper -p 2181:2181 jplock/zookeeper:3.4.6
 docker run -d --name kafka --link zookeeper:zookeeper -p 9092:9092 ches/kafka
 sleep 3s
-export ZK_IP=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' zookeeper)
-docker run --rm ches/kafka kafka-topics.sh --create --topic test --replication-factor 1 --partitions 1 --zookeeper $ZK_IP
+docker run --net=host --rm ches/kafka kafka-topics.sh --create --topic test --replication-factor 1 --partitions 1 --zookeeper localhost
